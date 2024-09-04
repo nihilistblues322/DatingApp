714ff916001f201694ada79bc07cd899fb7879f3
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-09-04 16:13:37 +0500

    auth guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -11,21 +12,29 @@ export const routes: Routes = [
         component: HomeComponent,
     },
     {
-        path: 'members',
-        component: MemberListComponent,
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            {
+                path: 'members',
+                component: MemberListComponent,
+            },
+            {
+                path: 'members/:id',
+                component: MemberDetailComponent,
+            },
+            {
+                path: 'lists',
+                component: ListsComponent,
+            },
+            {
+                path: 'messages',
+                component: MessagesComponent,
+            },
+        ],
     },
-    {
-        path: 'members/:id',
-        component: MemberDetailComponent,
-    },
-    {
-        path: 'lists',
-        component: ListsComponent,
-    },
-    {
-        path: 'messages',
-        component: MessagesComponent,
-    },
+
     {
         path: '**',
         component: HomeComponent,
