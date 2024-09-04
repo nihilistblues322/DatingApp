69c1b568ce2540dd75f470ecaee3ae64f277db93
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-09-04 15:56:24 +0500

    new components, toastr, routes

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-register',
@@ -11,6 +12,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
     private accountService = inject(AccountService);
+    private toastr = inject(ToastrService);
 
     cancelRegister = output<boolean>();
     model: any = {};
@@ -21,7 +23,7 @@ export class RegisterComponent {
                 console.log(res);
                 this.cancel();
             },
-            error: (err) => console.log(err),
+            error: (err) => this.toastr.error(err.error),
         });
     }
 
