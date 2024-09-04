64520302c3a3354e7d0b4b80ba58dd724b2152b8
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-09-05 00:33:58 +0500

    exception middleware, api exception

--- API/DTO/RegisterDto.cs
+++ API/DTO/RegisterDto.cs
@@ -9,9 +9,10 @@ namespace API.DTO
     public class RegisterDto
     {
         [Required]
-        public required string UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
 
         [Required]
-        public required string Password { get; set; }
+        [StringLength(8, MinimumLength = 4)]
+        public string Password { get; set; } = string.Empty;
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(policy =>
     policy
         .AllowAnyHeader()
