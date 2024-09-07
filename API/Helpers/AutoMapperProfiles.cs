a142a0dffe1589342b00e570704442de15003a1f
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-09-07 21:48:25 +0500

    auto mapper changes, user controller, user repo

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.DTO;
 using API.Entities;
+using API.Extensions;
 using AutoMapper;
 
 namespace API.Helpers
@@ -13,6 +14,7 @@ namespace API.Helpers
         public AutoMapperProfiles()
         {
             CreateMap<AppUser, MemberDto>()
+                .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
                 .ForMember(
                     d => d.PhotoUrl,
                     o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url)
