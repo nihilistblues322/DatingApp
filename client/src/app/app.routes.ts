c965a0b0b8cede3951042bcc83d121aeb6c7cc5c
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-09-08 22:55:15 +0500

    member detail tabs, gallery

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -23,7 +23,7 @@ export const routes: Routes = [
                 component: MemberListComponent,
             },
             {
-                path: 'members/:id',
+                path: 'members/:username',
                 component: MemberDetailComponent,
             },
             {
