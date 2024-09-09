9bdee5517d063cb63821c08fdac101d28f049cb3
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-09-09 22:56:44 +0500

    member edit, guard edit, browser notify

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 export const routes: Routes = [
     {
@@ -25,6 +27,11 @@ export const routes: Routes = [
             {
                 path: 'members/:username',
                 component: MemberDetailComponent,
+            },
+            {
+                path: 'member/edit',
+                component: MemberEditComponent,
+                canDeactivate: [preventUnsavedChangesGuard],
             },
             {
                 path: 'lists',
---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,58 @@
+import {
+    Component,
+    HostListener,
+    inject,
+    OnInit,
+    ViewChild,
+} from '@angular/core';
+import { IMember } from '../../_models/member';
+import { AccountService } from '../../_services/account.service';
+import { MembersService } from '../../_services/members.service';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+    selector: 'app-member-edit',
+    standalone: true,
+    imports: [TabsModule, FormsModule],
+    templateUrl: './member-edit.component.html',
+    styleUrl: './member-edit.component.css',
+})
+export class MemberEditComponent implements OnInit {
+    @HostListener('window:beforeunload', ['$event']) unloadNotification(
+        $event: any
+    ) {
+        if (this.editForm?.dirty) {
+            $event.returnValue = true;
+        }
+    }
+    @ViewChild('editForm') editForm?: NgForm;
+    member?: IMember;
+    private accountService = inject(AccountService);
+    private memberService = inject(MembersService);
+    private toastr = inject(ToastrService);
+
+    ngOnInit(): void {
+        this.loadMember();
+    }
+
+    loadMember() {
+        const user = this.accountService.currentUser();
+        if (!user) return;
+        this.memberService.getMember(user.userName).subscribe({
+            next: (member) => {
+                this.member = member;
+            },
+            error: (err) => {
+                console.log(err);
+            },
+        });
+    }
+
+    updateMember() {
+        console.log(this.member);
+        this.toastr.success('Profile updated successfully');
+        this.editForm?.reset(this.member);
+    }
+}
