f7dcafa1937dca82f559f1898b27ddcd5df9e806
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-09-11 17:12:20 +0500

    cloudinary service + settings

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -4,30 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Data.Repositories;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Extensions
 {
-	public static class ApplicationServiceExtensions
-	{
-		public static IServiceCollection AddApplicationServices(
-			this IServiceCollection services,
-			IConfiguration config
-		)
-		{
-			services.AddControllers();
-			services.AddDbContext<DataContext>(options =>
-			{
-				options.UseSqlite(config.GetConnectionString("DefaultConnection"));
-			});
-			services.AddCors();
-			services.AddScoped<ITokenService, TokenService>();
-			services.AddScoped<IUserRepository, UserRepository>();
-			services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(
+            this IServiceCollection services,
+            IConfiguration config
+        )
+        {
+            services.AddControllers();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPhotoService, PhotoService>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
-			return services;
-		}
-	}
+            return services;
+        }
+    }
 }
